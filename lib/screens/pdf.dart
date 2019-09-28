7124aeab79e5b34468e5822de51c73abd85ab2de
viktor-laptop <viksa_1998@hotmail.com>
2019-09-28 18:49:01 +0200

    viktor - commit za antonio da raboti

---
+++ lib/screens/pdf.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'dart:typed_data';
+
+PictureRecorder pictureRecorder = new PictureRecorder();
+Canvas can = new Canvas(pictureRecorder);
