8c2fb9140250e4873684541accb4c915358b0516
Антонио Аврамовиќ <Antonio.avramovic@gmail.com>
2019-09-29 09:56:11 +0200

    style change for circumstances try confirmation popup buttons work

---
+++ lib/screens/confirmation_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:insurhack_izveshtaj_app/main.dart';
+
+
+class Confirmation extends StatefulWidget {
+  @override
+  _ConfirmationState createState() => _ConfirmationState();
+}
+
+class _ConfirmationState extends State<Confirmation> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextFormField(),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: RaisedButton(
+                              child: Text("Submit"),
+                              onPressed: () {
+                                return MyApp();
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                });
+          },
+          child: Text("Open Popup"),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/prestart_check_screen.dart
+++ lib/screens/prestart_check_screen.dart
@@ -4,6 +4,7 @@ import 'package:insurhack_izveshtaj_app/main.dart';
 import 'package:insurhack_izveshtaj_app/screens/circumstances_of_the_accident_screen.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
+
 class PreStartCheck extends StatefulWidget {
   static const routeName = '/prestart_check';
 
@@ -18,9 +19,9 @@ class _PreStartCheckState extends State<PreStartCheck> {
   var _driveable = false;
   var _plates = false;
   var _licence = false;
-  static const _colorPrimary = Color.fromRGBO(0,145,100, 1);
-  static const _colorSecondary = Color.fromRGBO(204,0,64, 1);
-  static const _colorTertiary = Color.fromRGBO(0,0,120,1);
+  static const _colorPrimary = Color.fromRGBO(0, 145, 100, 1);
+  static const _colorSecondary = Color.fromRGBO(204, 0, 64, 1);
+  static const _colorTertiary = Color.fromRGBO(0, 0, 120, 1);
   static const _colorRed = Color.fromRGBO(245, 7, 7, 1);
   static const _colorWhite = Color.fromRGBO(255, 255, 255, 1);
 
@@ -37,97 +38,143 @@ class _PreStartCheckState extends State<PreStartCheck> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Околности"),
-        backgroundColor: Color.fromRGBO(0, 204, 140, 1),
-      ),
-      body: Container(
-        child: Column(
+        appBar: AppBar(
+          title: Text("Околности"),
+          backgroundColor: Color.fromRGBO(0, 204, 140, 1),
+        ),
+        body: Container(
+            child: Column(
           children: <Widget>[
             ListView(
               shrinkWrap: true,
               children: <Widget>[
                 SwitchListTile(
-                  title: const Text('Дали некој од учесниците е во алкохолизирана состојба?'),
+                  title: const Text(
+                      'Дали некој од учесниците е во алкохолизирана состојба?'),
                   value: _alcohol,
-                  onChanged: (bool value) { setState(() { _alcohol = value; }); },
-                  secondary: const Icon(Icons.battery_full, color: _colorPrimary),
+                  onChanged: (bool value) {
+                    setState(() {
+                      _alcohol = value;
+                    });
+                  },
+                  secondary:
+                      const Icon(Icons.battery_full, color: _colorPrimary),
                   activeColor: _colorRed,
                 ),
                 SwitchListTile(
                   title: const Text('Дали има повредени?'),
                   value: _injured,
-                  onChanged: (bool value) { setState(() { _injured = value; }); },
+                  onChanged: (bool value) {
+                    setState(() {
+                      _injured = value;
+                    });
+                  },
                   secondary: const Icon(Icons.add, color: _colorPrimary),
                   activeColor: _colorRed,
                 ),
                 SwitchListTile(
-                  title: const Text('Дали некое од возилата е во неподвижна состојба?'),
+                  title: const Text(
+                      'Дали некое од возилата е во неподвижна состојба?'),
                   value: _driveable,
-                  onChanged: (bool value) { setState(() { _driveable = value; }); },
-                  secondary: const Icon(Icons.directions_car, color: _colorPrimary),
+                  onChanged: (bool value) {
+                    setState(() {
+                      _driveable = value;
+                    });
+                  },
+                  secondary:
+                      const Icon(Icons.directions_car, color: _colorPrimary),
                   activeColor: _colorRed,
                 ),
                 SwitchListTile(
-                  title: const Text('Дали некое од возилата е со странски регистарски таблици?'),
+                  title: const Text(
+                      'Дали некое од возилата е со странски регистарски таблици?'),
                   value: _plates,
-                  onChanged: (bool value) { setState(() { _plates = value; }); },
-                  secondary: const Icon(Icons.local_car_wash, color: _colorPrimary),
+                  onChanged: (bool value) {
+                    setState(() {
+                      _plates = value;
+                    });
+                  },
+                  secondary:
+                      const Icon(Icons.local_car_wash, color: _colorPrimary),
                   activeColor: _colorRed,
                 ),
                 SwitchListTile(
-                  title: const Text('Дали некој од возачите е без возачка дозвола?'),
+                  title: const Text(
+                      'Дали некој од возачите е без возачка дозвола?'),
                   value: _licence,
-                  onChanged: (bool value) { setState(() { _licence = value; }); },
-                  secondary: const Icon(Icons.credit_card, color: _colorPrimary),
+                  onChanged: (bool value) {
+                    setState(() {
+                      _licence = value;
+                    });
+                  },
+                  secondary:
+                      const Icon(Icons.credit_card, color: _colorPrimary),
                   activeColor: _colorRed,
                 ),
               ],
             ),
           ],
+        )),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor:
+              !enableCallButton() ? _colorPrimary : _colorSecondary,
+          key: globalKey,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: _colorWhite),
+              title: new Text(
+                'Излези',
+                style: TextStyle(color: _colorWhite),
+              ),
+            ),
+            enableCallButton()
+                ? BottomNavigationBarItem(
+                    icon: Icon(Icons.call, color: _colorWhite),
+                    title: new Text(
+                      'Повикај полиција',
+                      style: TextStyle(color: _colorWhite),
+                    ),
+                  )
+                : BottomNavigationBarItem(
+                    backgroundColor: _colorWhite,
+                    icon: Icon(
+                      FontAwesomeIcons.chevronRight,
+                      color: _colorWhite,
+                    ),
+                    title: new Text(
+                      'Продолжи',
+                      style: TextStyle(color: _colorWhite),
+                    ),
+                  )
+          ],
+          onTap: (int index) {
+            setState(() {
+              navigationIndex = index;
+              switch (navigationIndex) {
+                case 0:
+                  Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                    return MyApp();
+                  }));
+                  break;
+                case 1:
+                  if (enableCallButton()) {
+                    _launchCaller();
+                  }
+                  else{
+                    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+                      return CircumstancesOfTheAccidentScreen();
+                    }));
+                  }
+                  break;
+              }
+            });
+          },
         )
-      ),bottomNavigationBar:BottomNavigationBar(
-      type: BottomNavigationBarType.fixed ,
-      backgroundColor: !enableCallButton() ? _colorPrimary : _colorSecondary,
-      key: globalKey,
-      items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home,color: _colorWhite),
-          title: new Text('Излези',style: TextStyle(color:_colorWhite ),),
-        ),
-        enableCallButton()?
-        BottomNavigationBarItem(
-          icon: Icon(Icons.call,color: _colorWhite),
-          title: new Text('Повикај полиција',style: TextStyle(color:_colorWhite ),),
-        ):
-        BottomNavigationBarItem(
-          backgroundColor: _colorWhite,
-          icon: Icon(FontAwesomeIcons.chevronRight  , color: _colorWhite,),
-          title: new Text('Продолжи',style: TextStyle(color:_colorWhite ),),
-        )
-      ],
-      onTap: (int index) {
-        setState(() {
-          navigationIndex = index;
-          switch (navigationIndex ) {
-            case 0:
-              Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-                return MyApp();
-              }));
-              break;
-            case 1:
-              _launchCaller();
-              break;
-            case 2:
-
-              break;
-          }
-        });
-      },
-    )
     );
   }
 }
+
 _launchCaller() async {
   const url = "tel:192";
   if (await canLaunch(url)) {
